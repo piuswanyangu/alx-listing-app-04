cd210210a21c11d0f6c30a14d5402c6adedb2faf
pius wanyangu <ndubipius96@gmail.com>
2026-01-19 23:24:06 +0300

    API integration for the property Details page

--- pages/property/[id].tsx
+++ pages/property/[id].tsx
@@ -1,17 +1,35 @@
-import { PROPERTYLISTINGSAMPLE } from "@/constants";
+// import { PROPERTYLISTINGSAMPLE } from "@/constants";
 import { useRouter } from "next/router";
+import axios from "axios";
 import  PropertyDetail from '@/components/property/PropertyDetail';
+import { useEffect, useState } from "react";
 
 export default function PropertyPage() {
-    const router = useRouter();
-    const { id } = router.query;
-    const property = PROPERTYLISTINGSAMPLE.find((item) => item.name === id);
+   const router = useRouter();
+   const { id } = router.query;
+   const [ property, setProperty] = useState(null);
+   const [loading, setLoading] = useState(true)
 
-    if (!property) return <p>Property not found</p>;
+   useEffect(() => {
+    const fetchProperty = async () => {
+        if(!id) return;
+        try {
+            const response = await axios.get(`/api/properties/${id}`)
+            setProperty(response.data)
+        } catch (error) {
+            console.error("Error fetching property details", error)
+        } finally {
+            setLoading(false)
+        }
+    };
 
-    return (
-        <div>
-            <PropertyDetail property={property} />
-        </div>
-    )
+    fetchProperty()
+   }, [id]);
+   if(loading){
+    return <p>Loading...</p>
+   }
+   if(!property) {
+    return <p>Property not found</p>
+   }
+    return <PropertyDetail property={property}/>
 }
